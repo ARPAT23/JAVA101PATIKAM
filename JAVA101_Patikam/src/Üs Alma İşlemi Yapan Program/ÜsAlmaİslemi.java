4864925be66c3f90e78be34e3e1dde0ac8a468e6
Arpat23 <serghei.duloglu@bil.omu.edu.tr>
2023-10-13 22:14:43 +0300

    KODLUYORUZ

---
+++ JAVA101_Patikam/src/Üs Alma İşlemi Yapan Program/ÜsAlmaİslemi.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ÜsAlmaÝslemi {public static void main(String[] args) {
+    int number1,number2,total=1;
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Üssü alýnacak sayýyý giriniz :");
+    number1= input.nextInt();
+
+    System.out.println("Üs olacak sayýyý giriniz :");
+    number2= input.nextInt();
+
+    for(int i = 1; i <= number2 ;i++){
+        total*=number1;
+    }
+    System.out.println("Sonuç : " + total);
+
+ }
+}
