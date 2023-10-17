07dd54150b57d35e6818b4ce4a399233548e21f4
Arpat23 <serghei.duloglu@bil.omu.edu.tr>
2023-10-17 20:42:54 +0300

    KODLUYORUZ

---
+++ JAVA101_Patikam/src/Recursive Metotlar ile Üslü Sayı Hesaplama/RecursiveMetotlarİleÜslüSayiHesaplama.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class RecursiveMetotlarileÜslüSayiHesaplama {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Taban deðeri giriniz: ");
+        int taban = scanner.nextInt();
+        System.out.print("Üs deðerini giriniz: ");
+        int üs = scanner.nextInt();
+        
+        System.out.println(taban + "^" + üs + " = " + üsalma(taban, üs));
+    }
+
+    static int üsalma(int taban, int üs){
+        if(üs == 0) return 1;
+        else return taban * üsalma(taban, üs - 1);
+   }
+
+}
+
+
